51e48d68aec969f31444337e45417d369af93da5
hndd0725 <hndd0725@naver.com>
2023-09-21 12:21:07 +0900

    xy위치에 캐릭터그려줄 수 있는 함수추가완료

--- character_grass.py
+++ character_grass.py
@@ -11,30 +11,32 @@ grass.draw_now(400,30)
 character.draw_now(300,90)
 delay(1)
 
+def render_frame(x,y):
+        clear_canvas_now()
+        grass.draw_now(400,30)
+        character.draw_now(x,y)
+        delay(0.1)
+    
+
 def run_circle():
     print('CIRCLE')
     cx,cy,r=400,300,200
     for deg in range(0,360,5):
         x=cx+r*math.sin(math.radians(deg))
         y=cy+r*math.cos(math.radians(deg))
-        clear_canvas_now()
-        grass.draw_now(400,30)
-        character.draw_now(x,y)
-        delay(0.1)
+        render_frame(x,y)
    
 
 def run_rectangle():
     print('RECTANGLE')
     #bottom line
     for x in range(50,750+1,5):
-        clear_canvas_now()
-        grass.draw_now(400,30)
-        character.draw_now(x,90)
-        delay(0.1)
+        render_frame(x,90)#xy위치에 캐릭터그려줄 수 있는
+        
     
     
 while True:
-    #run_circle()
+    run_circle()
     run_rectangle()
 
 close_canvas()
